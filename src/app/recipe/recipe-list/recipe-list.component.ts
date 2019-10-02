cb63be6c8466f07f785f63080b661728e8bec75d
Chris Martin <chris.bizunit34@gmail.com>
2019-10-01 23:41:38 -0400

    have inital app setup complete

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+  new Recipe('Steak', 
+  'This recipe allows you to prepare a delicous steak in an hour or less of cook and prep time!', 
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO1sxSHX_cyMc_7Z7j03WdzRAO5_8gCKkwLMUcmPyLxldpmjCJ8A'),
+  new Recipe('Rib Eye', 'This recipe gives the perfect prep time for a rib eye steak', 
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIx8XsrdDYJ8tAjO9HlXTO12rhnYjskSPsMlJirox4wBKQKc4H')
+  ];
 
   constructor() { }
 
